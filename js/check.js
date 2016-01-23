50dfab0343ab25b814235fdf1fcb03e19265e007
birra05 <birra05@mail.ru>
2016-01-23 20:08:10 +0300

    Попытка сделать модуль 2

---
+++ js/check.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function getMessage(a,b){
+  if (typeof(a) == 'boolean') {
+    if(a) {
+      return ('Я попал в' +b);
+    }
+
+    else {
+      return ('Я никуда не попал');
+    }
+  }
+
+  if (typeof(a) == 'number') {
+    return ('Я прыгнул на '+a*100+' сантиметров');
+  }
+
+  if (a instanceof Array && !(b instanceof Array)){
+    var sum=0;
+    for (var i = 0; i <a.length; i++) {
+      sum=sum+a[i];
+    };
+    return "Я прошёл "+sum+" шагов";
+  }
+
+  if (a instanceof Array && b instanceof Array ){
+    var length=0;
+    for (var i = 0; i <a.length; i++) {
+      length=length+(a[i]*b[i]);
+    };
+    return "Я прошёл "+length+" метров";
+  }
+}

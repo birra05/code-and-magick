ef90f11a6d928b973957a274824413a1dc0f29b9
birra05 <birra05@mail.ru>
2016-01-25 21:35:46 +0300

    Правки

--- js/check.js
+++ js/check.js
@@ -1,33 +1,41 @@
-"use strict";
+'use strict';
 
 function getMessage(a,b){
-  if (typeof(a) == "boolean") {
+  if (a === true) {
     if(a) {
-      return ("Я попал в" + b);
+      return ('Я попал в' + b);
     }
 
     else {
-      return ("Я никуда не попал");
+      return ('Я никуда не попал');
     }
   }
 
-  if (typeof(a) == "number") {
-    return ("Я прыгнул на " + a*100 + " сантиметров");
+  if (typeof(a) == 'number') {
+    if(a) {
+      return ('Я прыгнул на ' + a*100 + ' сантиметров');
+    }
+
+    else {
+
+    }
   }
 
-  if (a instanceof Array && !(b instanceof Array)){
-    var sum=0;
-    for (var i = 0; i <a.length; i++) {
-      sum=sum+a[i];
-    };
-    return "Я прошёл " + sum + " шагов";
-  }
+  if (a instanceof Array && b instanceof Array) {
+    if(a) {
+      var length = 0;
+      for (var i = 0; i <a.length; i++) {
+        length = length + (a[i]*b[i]);
+      };
+      return ('Я прошёл ' + length + ' метров');
+    }
 
-  if (a instanceof Array && b instanceof Array ){
-    var length=0;
-    for (var i = 0; i <a.length; i++) {
-      length=length+(a[i]*b[i]);
-    };
-    return "Я прошёл " + length + " метров";
+    else (a instanceof Array) {
+      var sum = 0;
+      for (var i = 0; i <a.length; i++) {
+        sum = sum + a[i];
+      };
+      return ('Я прошёл ' + sum + ' шагов');
+    }
   }
 }

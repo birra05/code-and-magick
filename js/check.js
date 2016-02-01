7de84f1a94dd09057597f412ea2f438fc242effd
birra05 <birra05@mail.ru>
2016-02-01 23:16:28 +0300

    Еще немного правок

--- js/check.js
+++ js/check.js
@@ -1,51 +1,31 @@
 'use strict';
 
-function getMessage(a,b){
+function getMessage(a,b) {
   if (a === true) {
-    if(a) {
-      return ('Я попал в' + b);
-    }
-
-    else {
-      return ('Я никуда не попал');
-    }
+    return ('Я попал в' + b);
   }
 
-  if (typeof(a) == 'number') {
-    if(a) {
-      return ('Я прыгнул на ' + a*100 + ' сантиметров');
-    }
-
-    else {
-
-    }
+  else if (a === false) {
+    return ('Я никуда не попал');
   }
 
-  if (a instanceof Array && !(b instanceof Array)) {
-    if(a) {
-      var sum = 0;
-      for (var i = 0; i <a.length; i++) {
-        sum = sum + a[i];
-      };
-      return ('Я прошёл ' + sum + ' шагов');
-    }
-    
-    else {
-
-    }
+  else if (typeof(a) == 'number') {
+    return ('Я прыгнул на ' + a*100 + ' сантиметров');
   }
 
-  if (a instanceof Array && b instanceof Array) {
-    if(a) {
-      var length = 0;
-      for (var i = 0; i <a.length; i++) {
-        length = length + (a[i]*b[i]);
-      };
-      return ('Я прошёл ' + length + ' метров');
-    }
+  else if (a instanceof Array && b instanceof Array) {
+    var length = 0;
+    for (var i = 0; i <a.length; i++) {
+      length = length + (a[i]*b[i]);
+    };
+    return ('Я прошёл ' + length + ' метров');
+  }
 
-    else {
-
-    }
+  else if (a instanceof Array) {
+    var sum = 0;
+    for (var i = 0; i <a.length; i++) {
+      sum = sum + a[i];
+    };
+    return ('Я прошёл ' + sum + ' шагов');
   }
 }

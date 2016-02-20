903231bb6203636443f489c7b94ea05f152a69b9
birra05 <birra05@mail.ru>
2016-02-20 21:37:40 +0300

    templates

--- js/reviews.js
+++ js/reviews.js
@@ -1,3 +1,4 @@
+/*global reviews*/
 
 'use strict';
 
@@ -6,6 +7,33 @@
 
   reviewFilter.classList.add('invisible');
 
-  
+  var container = document.querySelector('.reviews-list');
+
+  reviews.forEach(function(review) {
+    var element = getElementFromTemplate(review);
+    container.appendChild(element);
+  });
+
+  // Получение данных из шаблона
+
+  function getElementFromTemplate(data) {
+    var template = document.querySelector('#review-template');
+    if ('content' in template) {
+      var element = template.content.children[0].cloneNode(true);
+    } else {
+      element = template.children[0].cloneNode(true);
+    }
+
+    var userImage = new Image();
+    var IMG_SIZE = 124;
+    userImage.src = data.author.picture;
+    userImage.width = IMG_SIZE;
+    userImage.height = IMG_SIZE;
+    userImage.title = data.author.name;
+    element.querySelector('.review-rating').textContent = data.rating;
+    element.querySelector('.review-text').textContent = data.description;
+
+    return element;
+  }
 
 })();
